4a101446ffab1e66b53744e19a6b2ac4a94977af
Naveen <naveen.ccmsd@gmail.com>
2025-04-09 00:11:34 +0530

    refactor firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'FirebaseOptions.dart';
+import 'config/FirebaseOptions.dart';
 import 'LoginScreen.dart';
 
 
